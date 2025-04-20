9ab649daabfe1d582c78d149c20fa34feef0622f
Guy Amzaleg <guyamzaleg@gmail.com>
2025-04-20 22:56:46 +0300

    all screens were added

---
+++ game.js
@@ -0,0 +1,7 @@
+function initGame() {
+    document.getElementById('output').innerText = "Game initialized. Welcome to the game!";
+    console.log("Game initialized.");
+    // Initialize game variables and state here
+}
+
+initGame();
